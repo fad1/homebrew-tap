47a7d72305ba44e54d209b69fcd8b2e6b150b195
Red Locomotive <fahd777@protonmail.com>
2026-01-24 12:48:38 +0100

    Update Switcher to v1.0.1

--- Casks/switcher.rb
+++ Casks/switcher.rb
@@ -1,6 +1,6 @@
 cask "switcher" do
-  version "1.0.0"
-  sha256 "9af96dd0a2d05ba47f5bdecee660e156b54b1a70ef0c302630f33f6ca8393b14"
+  version "1.0.1"
+  sha256 "5967159371e25b0c7d8570ea8f65c334147796e6aa3d0594c73b4b19e86f52ef"
 
   url "https://github.com/fad1/Switcher/releases/download/v#{version}/Switcher.zip"
   name "Switcher"
